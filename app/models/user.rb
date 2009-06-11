2dc14fc8d421e57f22faffc47665bd3c99af30d0
zhongxingdou <zhongxingdou@gmail.com>
2009-06-11 09:50:10 +0800

    updated: changed Array#find_index to Array#find

--- app/models/user.rb
+++ app/models/user.rb
@@ -56,7 +56,7 @@ class User < ActiveRecord::Base
     menu_modules = []
     pages.each do | p |
       m = p.page_module
-      unless menu_modules.find_index{ |menu_m| menu_m.id == m.id }
+      unless menu_modules.find{ |menu_m| menu_m.id == m.id }
         menu_modules << MenuModule.new(m, pages.collect{|p| p if(p.page_module == m && !p.hidden)}.compact! ) 
       end
     end
