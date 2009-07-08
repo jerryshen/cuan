0b78556a19b155c072f32a38f76d7f96cf3ea528
jerry <hansay99@gmail.com>
2009-07-08 09:25:39 +0800

    data import

--- app/controllers/data_import_controller.rb
+++ app/controllers/data_import_controller.rb
@@ -3,23 +3,40 @@ class DataImportController < ApplicationController
   end
 
   def import
-    unless params[:excel_file].nil?
-      unless params[:category].empty?
-        begin
-          data = DataImport.collet_excel_data(params[:excel_file],params[:category])
-          unless data.nil?
-            render :text => DataImport.print_data(data)
-          else
-            render :text => "导入数据失败"
-          end
-        rescue => error
-          render :text => error.to_s
+    if params[:excel_file].nil?
+      return render :text => "未选择导入文件"
+    end
+    if params[:category].empty?
+      return render :text => "必须选择文件数据类别"
+    end    
+    if params[:year].empty?
+      return render :text => "年份不能为空"
+    end
+    if params[:month].empty?
+      return render :text => "月份不能为空"
+    end    
+    
+    begin
+      success = DataImport.collet_excel_data(params[:excel_file],params[:category],params[:year],params[:month])
+      if success
+        case params[:category].to_i
+        when 1 #财政在职工资
+          return redirect_to :controller => 'temp5s', :action => 'index'
+        when 2 #在职学院工资
+          return redirect_to :controller => 'temp2s', :action => 'index'
+        when 3 #离休人员工资
+          return redirect_to :controller => 'temp1s', :action => 'index'
+        when 4 #退休人员工资
+          return redirect_to :controller => 'temp3s', :action => 'index'
+        when 5 #退休人员学院工资
+          return redirect_to :controller => 'temp4s', :action => 'index'
         end
       else
-        render :text => "必须选择文件数据类别"
+        render :text => "导入数据失败"
       end
-    else
-      render :text => "未选择导入文件"
+    rescue => error
+      render :text => error.to_s
     end
+      
   end
 end
--- app/models/data_import.rb
+++ app/models/data_import.rb
@@ -1,22 +1,22 @@
-#require 'excelrill'
-require 'excelrillinux' #winOLE less
+require 'excelrill'
+#require 'excelrillinux' #winOLE less
 require 'file_upload_util'
 class DataImport
-  def self.collet_excel_data(file,category)
+  def self.collet_excel_data(file, category, year, month)
     file_path = upload_to_temp_dir(file)
     data = nil
     begin
       case category
       when "1"
-        data = import_cz_zz(file_path)
+        data = import_cz_zz(file_path, year, month)
       when "2"
-        data = import_zz_xy(file_path)
+        data = import_zz_xy(file_path, year, month)
       when "3"
-        data = import_lx_ry(file_path)
+        data = import_lx_ry(file_path, year, month)
       when "4"
-        data = import_tx_ry(file_path)
+        data = import_tx_ry(file_path, year, month)
       when "5"
-        data = import_tx_ry_xy(file_path)
+        data = import_tx_ry_xy(file_path, year, month)
       end
     rescue => error
       raise error
@@ -102,61 +102,92 @@ class DataImport
     return as.join("\n")
   end
 
-  #离休人员工资
-  def self.import_lx_ry(path)
+  #离休人员工资导入 --> Temp1表
+  def self.import_lx_ry(path, year, month)
     option = {:data_area=>{:start_row => 4,:start_column => 1, :end_column => -4}}
     option[:sheet_index] = 1
-    option[:keys] =         %w(姓名	月基本离休费	工改保留补贴_93年	其他国家出台津补贴	地方出台津补贴	补发工资	扣水费)
+    option[:keys] =          %w(f1	    f2          	f3	              f4	                f5	            f6	      f7)
+    #option[:keys] =         %w(姓名	月基本离休费	工改保留补贴_93年	其他国家出台津补贴	地方出台津补贴	补发工资	扣水费)
 #RetiredBasicSalaryRecord   user_id basic_fee     stay_be           foreign_be          region_be        null     null
     data = import_salary(path,option)
-    data.delete_if{ |row| row["姓名"].to_i > 0 }
+    data.delete_if{ |row| row["f1"].to_i > 0 } #f1 => 姓名
+    data.each do |row|
+      row["year"] = year
+      row["month"] = month
+    end
+    Temp1.create(data)
   end
 
 
-  #在职学院工资
-  def self.import_zz_xy(path)
+  #在职学院工资导入 --> temp2表
+  def self.import_zz_xy(path, year, month)
     option = {:data_area=>{:start_row => 1,:start_column => 2, :end_column => -2}}
     option[:month_cell]=nil
     option[:sheet_index]=0
-    option[:keys]= %w(姓名	$日期  岗位工资	 薪级工资	艰苦边远地区津贴	工改保留补贴	其他国家出台津贴	地方出台津贴	扣发工资	补发工资	生活补贴	补贴补差	活工资补差	电视补贴	驻蓉补贴	$应发工资  其他1	住房公积金	职工医疗保险费	职工个人教育费	个人所得税	扣水电费	扣失业保险费	其他扣款一	其他扣款二	其他扣款三) #BasicSalaryRecord user_id  null  station_sa position_sa       hard_be  stay_be       foreign_be        region_be     null      add_sa 
+    option[:keys]= %w(f1	  f2	   f3	       f4	      f5	              f6	          f7	              f8	          f9	      f10	      f11	      f12	      f13	        f14	      f15	      f16	       f17	  f18	        f19	            f20	            f21	        f22	      f23	          f24         f25         f26)
+    #option[:keys]= %w(姓名	$日期  岗位工资	 薪级工资	艰苦边远地区津贴	工改保留补贴	其他国家出台津贴	地方出台津贴	扣发工资	补发工资	生活补贴	补贴补差	活工资补差	电视补贴	驻蓉补贴	$应发工资  其他1	住房公积金	职工医疗保险费	职工个人教育费	个人所得税	扣水电费	扣失业保险费	其他扣款一	其他扣款二	其他扣款三) 
+#BasicSalaryRecord user_id  null  station_sa position_sa       hard_be  stay_be       foreign_be        region_be     null      add_sa 
 #CollegeBeRecord                                                                                                                          life_be   diff_be   livesa_be   tv_be    beaulty_be           other_be
 #FeeCuttingRecord                                                                                                                                                                                               room_fee    med_fee         selfedu_fee      self_tax   elc_fee   job_fee       other_fee1  other_fee2  other_fee3
     data=import_salary(path,option)
+    data.each do |row|
+      row["year"] = year
+      row["month"] = month
+    end    
+    Temp2.create(data)
   end
   
 
-  #退休人员工资
-  def self.import_tx_ry(path)
+  #退休人员工资导入 --> temp3表
+  def self.import_tx_ry(path, year, month)
     option = {:data_area=>{:start_row => 4,:start_column => 1, :end_column => -4}}
     option[:sheet_index] = 1
-    option[:keys] =        %w(姓名	月基本退休费	工改保留补贴_93年	其他国家出台津补贴	地方出台津补贴  $扣水费	其他扣款二	其他扣款三)
+    option[:keys] =         %w(f1   f2            f3                f4                  f5              f6      f7          f8)
+    #option[:keys] =        %w(姓名	月基本退休费	工改保留补贴_93年	其他国家出台津补贴	地方出台津补贴  $扣水费	其他扣款二	其他扣款三)
 #RetiredBasicSalaryRecord  user_id  basic_fee     stay_be           foreign_be          region_be       null 
 #RetiredFeeCuttingRecord                                                                                        other_fee2  other_fee3 
     data=import_salary(path,option)
-    data.delete_if{ |row| row["姓名"].to_i > 0 }
+    data.delete_if{ |row| row["f1"].to_i > 0 } #f1 => 姓名
+    data.each do |row|
+      row["year"] = year
+      row["month"] = month
+    end    
+    Temp3.create(data)
   end
 
 
-  #退休人员学院工资
-  def self.import_tx_ry_xy(path)
+  #退休人员学院工资导入 --> temp4表
+  def self.import_tx_ry_xy(path, year, month)
     option = {:data_area=>{:start_row => 1,:start_column => 1, :end_column => -2}}
     option[:month_cell]=nil
     option[:sheet_index]=0
-    option[:keys]=         %w(姓名	日期	补贴补差	电视补贴	驻蓉补贴	其他一	其他二	其他三	$扣水电费	其他扣款一	其他扣款二	其他扣款三)
+    option[:keys]=          %w(f1   f2    f3        f4        f5        f6      f7      f8      f9        f10         f11         f12)
+    #option[:keys]=         %w(姓名	日期	补贴补差	电视补贴	驻蓉补贴	其他一	其他二	其他三	$扣水电费	其他扣款一	其他扣款二	其他扣款三)
 #RetiredCollegeBeRecord    user_id  null  diff_be   tv_be   beaulty_be other_be1 other_be2 other_be3
 #RetiredFeeCuttingRecord                                                                                  other_fee1 other_fee2  other_fee3 
     data=import_salary(path,option)
+    data.each do |row|
+      row["year"] = year
+      row["month"] = month
+    end    
+    Temp4.create(data)
   end
 
-  #财政在职工资
-  def self.import_cz_zz(path)
+  #财政在职工资导入 --> temp5表
+  def self.import_cz_zz(path, year, month)
     option = {:data_area=>{:start_row=>4,:start_column => 1, :end_column => -4}}
     option[:sheet_index] = 1
-    option[:keys] = %w(姓名	岗位工资	技术等级（职务）工资	岗位津贴	其他国家出台津补贴	地方出台津补贴	补发工资	扣住房公积金	扣职工医疗保险费	扣水费	扣失业保险	其他扣款一	其他扣款二	其他扣款三)
+    option[:keys]=   %w(f1    f2        f3                    f4        f5                  f6              f7        f8            f9                f10     f11         f12         f13         f14)
+    #option[:keys] = %w(姓名	岗位工资	技术等级（职务）工资	岗位津贴	其他国家出台津补贴	地方出台津补贴	补发工资	扣住房公积金	扣职工医疗保险费	扣水费	扣失业保险	其他扣款一	其他扣款二	其他扣款三)
 #BasicSalaryRecord  user_id station_sa position_sa        station_sa  foreign_be         region_be        add_sa	
 #FeeCuttingRecord                                                                                                   room_fee      med_fee           elc_fee job_fee     other_fee1 other_fee2  other_fee3
     data = import_salary(path,option)
-    data.delete_if{ |row| row["姓名"].to_i > 0 }
+    data.delete_if{ |row| row["f1"].to_i > 0 } #f1 => 姓名
+    data.each do |row|
+      row["year"] = year
+      row["month"] = month
+    end    
+    Temp5.create(data)
   end
 
 end
